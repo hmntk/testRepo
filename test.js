4a2954c121e71c53f1daa70b5cf18d908e865ca5
srikanth061 <srikanthbachannagari@gmail.com>
2022-02-21 12:43:23 +0530

    test 2

--- test.js
+++ test.js
@@ -1,1 +1,43 @@
-console.log('hello changed by Hermanth');
+const express=require("express");
+const User = require("../model/userschema");
+const jwt=require("jsonwebtoken")
+const bcrypt=require("bcrypt")
+const router=express.Router();
+
+router.post("/",async function(req,res){
+    try{
+        const{id,password}=req.body;
+    const user=await User.findOne({id});
+    console.log("user",user)
+    if(!user){
+        res.json({
+            status:"failed",
+            message:"user not registred",
+            data:"error"
+        })
+    }
+    if(password!==user.password){
+            return res.json({
+                status:"failed",
+                message:"password incorrect",
+                data:'error'
+            });
+        }
+    const token=jwt.sign({
+        data: user._id,
+    }, 'secret');
+    res.json({
+        status:"success",
+        data:token
+    })
+
+    }catch(e){
+        return res.json({
+            status:"failed",
+            message:"internal error"
+        })
+
+    }
+    
+});
+module.exports=router;
